e8914bd707b0d54ec22086596afb7466458210e2
godfrey-creat <ogodfrey135@gmail.com>
2024-06-17 17:25:34 +0300

    openning port 5000

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ from app.app_factory import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    create_db()
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=5000
+    )
