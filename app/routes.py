440acc893aa8c1f3c627e8b9eff8855fb04491ee
godfrey-creat <ogodfrey135@gmail.com>
2024-06-18 11:09:04 +0300

    ok

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ import os
 from flask import Flask
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import LoginManager
-from flask_bcrypt import bcrypt
-from flask_bcypt import Bcrypt
+#from flask_bcrypt import bcrypt
+#from flask_bcypt import Bcrypt
 from flask_cors import (CORS, cross_origin)
 from flask_login import login_user, current_user, logout_user, login_required
 from .app_factory import db
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_KEY'] = 'manager_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///manager.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-bcrypt = Bcrypt(app)
+#bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
 db.init_app(app)
